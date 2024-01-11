4e97c2e739cf20be0741e1e31778aaba37b4cee0
s.kurochkin <semenkurochkin@gmail.com>
2024-01-11 23:00:22 +0300

    cart(mobile)

--- src/components/cart/script.js
+++ src/components/cart/script.js
@@ -1,4 +1,23 @@
 window.addEventListener('load', function (event) {
+    const options = {
+			rootMargin: '100px',
+			threshold: 0
+		};
+
+
+        const section = document.querySelector('.cart-checkout-sticky')
+        const bill = document.querySelector('.cart-bill')
+        const observer = new IntersectionObserver((entries, observer) => {
+            if (entries[0].isIntersecting) {
+                bill.classList.remove('active')
+
+            }else{
+                bill.classList.add('active')
+
+
+            }
+        }, options);
+        observer.observe(section);
 
 
 });
